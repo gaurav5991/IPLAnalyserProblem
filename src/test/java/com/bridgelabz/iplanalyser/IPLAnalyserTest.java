2da1298bc5f90416fd1287ed11ce0d5af65caa1a
gaurav5991 <gauravharendersharma@gmail.com>
2020-11-01 16:47:33 +0530

    Gaurav[ADDED]:Test case to know top batting average of cricketers

--- src/test/java/com/bridgelabz/iplanalyser/IPLAnalyserTest.java
+++ src/test/java/com/bridgelabz/iplanalyser/IPLAnalyserTest.java
@@ -2,6 +2,7 @@ package com.bridgelabz.iplanalyser;
 
 import com.bridgelabz.iplanalyser.service.IPLAnalyser;
 import com.bridgelabz.iplanalyser.Exception.IPLAnaylserException;
+import com.google.gson.Gson;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -25,16 +26,25 @@ public class IPLAnalyserTest {
         boolean message = iplAnalyser.printWelcomeMessage();
         Assert.assertTrue(message);
     }
-
+    /*Test Case to check the number of entries in Batting data csv file*/
     @Test
     public void givenBattingDataCSVFile_ShouldLoadBattingData() throws IPLAnaylserException {
         int totalRecords = iplAnalyser.loadBattingData(BATTING_DATA_PATH);
         assertEquals(100, totalRecords);
     }
-
+    /*Test Case to check the number of entries in Bowling data csv file*/
     @Test
     public void givenBowlingDataCSVFile_ShouldLoadBowlingData() throws IPLAnaylserException {
         int totalRecords = iplAnalyser.loadBowlingData(BOWLING_DATA_PATH);
         assertEquals(99, totalRecords);
+    }
+    /*Test Case to Sort the Batting Data by Average should return Highest Average*/
+    @Test
+    public void givenBattingData_WhenSortedByAvg_ShouldReturnHighestAvgFirst() throws IPLAnaylserException {
+        String sortedBattingData = "";
+        iplAnalyser.loadBattingData(BATTING_DATA_PATH);
+        sortedBattingData = iplAnalyser.battingAvgWiseSortedData();
+        Double[] battingData = new Gson().fromJson(sortedBattingData, Double[].class);
+        assertEquals(83.2, battingData[0].doubleValue(),0.0);
     }
 }
